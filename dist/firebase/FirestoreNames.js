9b86222b7a5ffe1185ead2a9c873465c45551b26
Nobody-123 <dikhyantkrishnadalai@gmail.com>
2022-10-28 22:38:49 +0530

    Added dist to gitignore

--- dist/firebase/FirestoreNames.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.firestoreNames = void 0;
-exports.firestoreNames = {
-    ALBUMS: "Albums",
-    SONGS: "Songs",
-};
